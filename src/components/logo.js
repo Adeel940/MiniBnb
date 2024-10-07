c41576859ac942663ba41198aec50efe6cf0ec60
Adeel940 <kbwadeel@gmail.com>
2024-10-08 01:14:00 +0500

    Add Logo and NavLinks

---
+++ src/components/logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="flex items-center">
+      <img
+        src="not crafted yet"
+        alt="airbnb"
+        className="h-8 w-8"
+      />
+      <span className="text-blue-400 text-xl font-bold ml-2">Minibnb</span>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/navLinks.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavLinks = () => {
+  return (
+    <div className="flex space-x-6 text-gray-700">
+      <a href="/" className="hover:text-black">Stays</a>
+      <a href="/" className="hover:text-black">Experiences</a>
+    </div>
+  );
+};
+
+export default NavLinks;
