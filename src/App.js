a2c89b84ba1776f1757496518822598a4c8f84ea
Adeel940 <kbwadeel@gmail.com>
2024-10-07 22:20:04 +0500

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className='text-yellow-400'>
           React App
         </p>
       </header>
